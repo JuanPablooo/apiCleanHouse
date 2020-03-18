6a71511c7fd2d3bfe65ac1b76b9dfc8ca1a5b3b7
sahAlves <sah.alves002@gmail.com>
2020-03-18 15:54:35 -0300

    Estruturando a classe Cliente

--- src/main/java/com/example/demo/model/Cliente.java
+++ src/main/java/com/example/demo/model/Cliente.java
@@ -1,28 +1,55 @@
 package com.example.demo.model;
 
+import lombok.Data;
 import org.hibernate.annotations.common.reflection.XMethod;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "tbl_clientes")
+@Data
 public class Cliente extends AtributosGenericos{
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    private String nome;
+    @NotNull
+    private String nomeCompleto;
+
+    @NotNull
+    private String dataNascimento;
+
+    @NotNull
+    private String email;
+
+    @NotNull
+    private String cpf;
+
+    private String fotoPerfil;
+
+    private String telefoneFixo;
+
+    @NotNull
+    private String celular;
+
+    @NotNull
+    @Size(min=8, max=20, message="A senha deve conter no mínimo 8 caracteres e no máximo 20!")
+    private String senha;
+
 
     @Override
     public String toString() {
-        return "Cliente{" +
-                ", nome='" + nome + '\'' +
-                '}';
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
+        return "Cliente[id=" + id +
+                ", nomeCompleto=" + nomeCompleto +
+                ", dataNascimento=" + dataNascimento +
+                ", email=" + email +
+                ", cpf=" + cpf  +
+                ", fotoPerfil=" + fotoPerfil +
+                ", telefoneFixo=" + telefoneFixo +
+                ", celular=" + celular +
+                "]";
     }
 }
