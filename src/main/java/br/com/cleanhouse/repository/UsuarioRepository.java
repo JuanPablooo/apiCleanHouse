6cdcebc834cfdeaf300a809667d689068c161b6c
sahAlves <sah.alves002@gmail.com>
2020-03-27 01:59:39 -0300

    tentando pegar o tipo de usuario

--- src/main/java/br/com/cleanhouse/repository/UsuarioRepository.java
+++ src/main/java/br/com/cleanhouse/repository/UsuarioRepository.java
@@ -6,8 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Usuario findByEmail(String email);
     boolean existsByEmail(String email);
-    Usuario findBySenha(String senha);
-    boolean existsBySenha(String senha);
-
     boolean existsByEmailAndSenha(String email, String senha);
 }
