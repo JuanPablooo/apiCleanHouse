0eab3be4c2dc1e23229cc21bc1948f8c060fe58e
sahAlves <sah.alves002@gmail.com>
2020-07-09 16:01:08 -0300

    Mudando retorno do login

--- src/main/java/br/com/cleanhouse/endpoint/ClienteEndPoint.java
+++ src/main/java/br/com/cleanhouse/endpoint/ClienteEndPoint.java
@@ -3,7 +3,6 @@ package br.com.cleanhouse.endpoint;
 
 import br.com.cleanhouse.error.ResourceNotFoundException;
 import br.com.cleanhouse.model.Cliente;
-import br.com.cleanhouse.model.ClienteDTO;
 import br.com.cleanhouse.model.Residencia;
 import br.com.cleanhouse.model.Usuario;
 import br.com.cleanhouse.repository.ClienteRepository;
@@ -15,15 +14,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.*;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
-import java.util.Set;
 
 @RestController
 @RequestMapping("v1")
@@ -63,14 +56,8 @@ public class ClienteEndPoint {
 
     @PostMapping(END_POINT)
     @Transactional(rollbackFor = Exception.class)
-    public ResponseEntity<?> setCliente(@Valid @RequestBody Cliente cliente, @RequestParam("file") MultipartFile file) throws IOException {
+    public ResponseEntity<?> setCliente(@Valid @RequestBody Cliente cliente){
         salvaUsuario(cliente.getUsuario());
-
-        byte[] bytes = file.getBytes();
-        Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
-        Files.write(path, bytes);
-
-        cliente.setFotoPerfil(file.getOriginalFilename());
 
         @Valid
         Usuario usuario =  cliente.getUsuario();
